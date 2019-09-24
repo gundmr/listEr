12743c62de6020931791fa45fdd70796f1d9b109
gundrm <rachelm.gundy@gmail.com>
2019-09-24 13:18:51 -0700

    enable delete with redux

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class List extends Component {
@@ -12,40 +12,26 @@ class List extends Component {
         this.props.getItems();
     }
 
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id);
+    };
+
     render() {
         
         const { items } = this.props.item;
         return(
             <Container>
-                <Button
-                    color="dark"
-                    style={{marginBottom: '2rem'}}
-                    onClick={() => {
-                        const name = prompt('Enter Item');
-                        if(name) {
-                            this.setState(state => ({
-                                items: [...state.items, { id: uuid(), name }]
-                            }));
-                        }
-                    }}>
-                    Add Item
-                    </Button>
-
                     <ListGroup>
                         <TransitionGroup className="shopping-list">
                         {items.map(({ id, name }) => (
                             <CSSTransition key={id} timeout={500} classNames="fade">
                                 <ListGroupItem>
-                {/* whatever item is clicked will be filtered out of the array */}
+
                                 <Button 
                                     className="remove-btn" 
                                     color="danger" 
                                     size="sm" 
-                                    onClick={() => {
-                                        this.setState(state => ({
-                                            items: state.items.filter(item => item.id !==id) 
-                                        }));
-                                    }}>
+                                    onClick={this.onDeleteClick.bind(this, id)}>
                                 &times;</Button>
                                     {name}
                                 </ListGroupItem>
@@ -70,4 +56,4 @@ const mapStateToProps = (state) => ({
     item: state.item
 });
 
-export default connect(mapStateToProps, { getItems })(List);
+export default connect(mapStateToProps, { getItems, deleteItem })(List);
