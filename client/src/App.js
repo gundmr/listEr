05f4939524710bdd3f93ddf6ae1b6c45b1724ee2
gundrm <rachelm.gundy@gmail.com>
2019-09-24 18:57:15 -0700

    auth enabled

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import List from './components/List';
 import ItemModal from './components/ItemModal';
@@ -6,23 +7,32 @@ import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        
-        <AppNavbar />
-        <Container>
-          <ItemModal />
-          <List />
-        </Container>
-      </div>
-    </Provider>
-  );
-}
+class App extends Component {
+
+  // function App() {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <List />
+          </Container>
+        </div>
+      </Provider>
+    );
+  };
+};
 
 export default App;
